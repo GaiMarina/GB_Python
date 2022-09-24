1dfab7cfac381120bc972a677f95c214a0d8a7f5
Marina <linamamarinina@gmail.com>
2022-09-24 21:37:27 +0300

    1 homework

--- Python_HomeWorks/1_HomeWork/truth_test.py
+++ Python_HomeWorks/1_HomeWork/truth_test.py
@@ -1,2 +1,11 @@
+
 # 2. Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
 #    для всех значений предикат. (¬ отрицание, ∧ и, ∨ или)
+
+x = float(input('Enter X: '))
+y = float(input('Enter Y: '))
+z = float(input('Enter Z: '))
+
+a = not(x or y or z)
+b = not x and not y and not z
+print(a == b)
