f40231379590523cc043fe057d9ffddecbc9550a
Marina <linamamarinina@gmail.com>
2022-10-01 13:56:27 +0300

    1 task, 3 HomeWork

---
+++ Python_HomeWorks/3_HomeWork/sum_odd_pos_dig_1.py
@@ -0,0 +1,24 @@
+
+# 1. Задайте список из нескольких чисел. 
+#    Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+#    Пример:
+#    [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+li_st = input("Enter numbers separated by space: ").split()
+print(f'The entered list is: {li_st}')
+
+temp = []
+s_um = 0
+
+for i in li_st[1::2]:
+    temp.append(str(i))
+    s_um += int(i)
+print('Numbers on the odd positions:', end = ' ')
+print(", ".join(temp))
+print(f'The sum of numbers with the odd indices equals: {s_um}')
+
+
+
+
+
+
