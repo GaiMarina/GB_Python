052d29c5d944d491b1d02904b6ed8213b8734c5c
Marina <linamamarinina@gmail.com>
2022-10-12 21:54:15 +0300

    RLE algoritm

--- Python_HomeWorks/5_HomeWork/algorithm_RLE_04.py
+++ Python_HomeWorks/5_HomeWork/algorithm_RLE_04.py
@@ -5,20 +5,63 @@
 from importlib.resources import path
 from itertools import groupby
 from itertools import starmap
+from operator import index
+from itertools import combinations
 
 
 def read_data(path):
     with open(path, 'r', encoding='utf-8') as data_file:
-        compr_data = data_file.read()
-    return compr_data
+        init_data = data_file.read()
+    return init_data
 
 
-path = '/home/marina/Documents/GeekBrains/GB_Python/Python_HomeWorks/5_HomeWork/data.txt'
-temp_result = read_data(path)
+def pack(path):
 
-for i, j in groupby(temp_result):
-    set_cut = [len(list(j)), i]
-    set_str = "".join(map(str, set_cut))
-    print(set_str, end='')
-    with open('compression_RLE.txt', 'a', encoding='utf-8') as the_file:
-        the_file.write(set_str)
+    temp_result = read_data(path)
+    pack_data = ''
+
+    for i, j in groupby(temp_result):
+        pack_data += str(len(list(j))) + str(i)
+    print(pack_data)
+
+    with open('compression_RLE.txt', 'w', encoding='utf-8') as the_file:
+        the_file.write(pack_data)
+
+    print('\n' + '-' * len(pack_data))
+
+
+def composition(cnt, s):
+    return int(cnt) * s
+
+
+def decoding(init_s):
+    set_list = []
+    a, b = '', ''
+    for i in init_s:
+        if i.isalpha():
+            b = i
+            set_list.append((a, b))
+            a, b = '', ''
+        else:
+            a += i
+
+    print(set_list)
+    return set_list
+
+
+def unpack(path):
+
+    temp_2_result = read_data(path)
+    unpack_data = ''
+
+    unpack_data = ''.join(starmap(composition, decoding(temp_2_result)))
+    print(unpack_data)
+
+    with open('uncompression_RLE.txt', 'w', encoding='utf-8') as the_file:
+        the_file.write(unpack_data)
+
+    print('\n' + '-' * len(unpack_data))
+
+
+pack('/home/marina/Documents/GeekBrains/GB_Python/Python_HomeWorks/5_HomeWork/data.txt')
+unpack('/home/marina/Documents/GeekBrains/GB_Python/compression_RLE.txt')
