4805e84051118f77060cfcfb0629d9aa98399fd0
Marina <linamamarinina@gmail.com>
2022-10-28 21:59:48 +0300

    9 HomeWork

--- Python_HomeWorks/9_HomeWork/bot_main.py
+++ Python_HomeWorks/9_HomeWork/bot_main.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 def log(update: Update, context: CallbackContext):
-    file = open('spy.csv', 'a')
+    file = open('db.csv', 'a')
     file.write(f'{update.effective_user.first_name},{update.effective_user.id}, {update.message.text}\n')
     file.close()
 
